759b9c35ec28e0b87eaad8628bf82451f7c5d88c
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-24 11:34:24 -0700

    Add find/delete location methods
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 there weill be a general location
 
 location will have properties - location, landmarks, time of year.
+full details of a location visit can be called upon with .fullDetails() method. 
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,41 @@
+function LocationBook() {
+  this.locations = [];
+  this.currentId = 0;
+}
+
+LocationBook.prototype.addLocation = function (location) {
+  location.id = this.assignId();
+  this.locations.push(location);
+}
+
+LocationBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+LocationBook.prototype.findLocation = function(id) {
+  for (let i=0; i< this.locations.length; i++) {
+    if (this.locations[i]) {
+      if (this.locations[i].id == id) {
+        return this.locations[i];
+      }
+    }
+  };
+  return false;
+}
+
+LocationBook.prototype.deleteLocation = function(id) {
+  for (let i=0; i< this.locations.length; i++) {
+    if (this.locations[i]) {
+      if (this.locations[i].id == id) {
+        delete this.locations[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
 //Business Logic for 
 function Location (locationName, landMarks, timeOfYear) {
   this.locationName = locationName;
